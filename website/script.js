20ac710410fd50e9eb8821b093c7caa87f777182
dis-craft <169169726+dis-craft@users.noreply.github.com>
2024-05-08 16:57:35 +0530

    Delete website directory

--- website/script.js
+++
@@ -1,9 +0,0 @@
-const menuLinks = document.querySelectorAll('nav a');
-
-menuLinks.forEach(link => {
-  if (link.href !== "projects.html") { // Exclude projects.html link
-    link.addEventListener('click', function(e) {
-      e.preventDefault(); // Prevent default behavior for other links
-    });
-  }
-});
